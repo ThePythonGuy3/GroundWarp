dd6b149cf8f07a1eed2105da205d6b7d49584bde
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2022-07-13 16:23:36 +0200

    bumpers, dinally

--- collisions.py
+++ collisions.py
@@ -1,17 +1,26 @@
 pg = None
 
 class collider:
-	def __init__(self, x, y, kill, size = 16):
+	def __init__(self, block, x, y, kill, size = 16, impulse = 0):
+		self.block = block
 		self.rect = pg.Rect(x * 16, y * 16, size, size)
 		self.kill = kill
+		self.impulse = impulse
 
 def rectCollide(colliderList, rect, ignore = False):
 	kill = False
 	collide = False
+	impulse = 0
+	impulseCol = None
+	impulseId = 0
 	for i in colliderList:
 		if rect.colliderect(i.rect):
-			if ignore and i.kill: continue
+			if ignore and (i.kill or i.impulse != 0): continue
 			collide = True
 			if i.kill: kill = True
-	return [collide, kill]
+			if i.impulse != 0:
+				impulse = i.impulse
+				impulseCol = i.block
+				impulseId = id(i)
+	return [collide, kill, impulse, impulseCol, impulseId]
 
